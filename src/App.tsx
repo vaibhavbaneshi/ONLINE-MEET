5f780fa42caa6db077e7f124a8037c1fb735ab6b
Vaibhav Baneshi <vaibhavbaneshi@gmail.com>
2024-03-26 12:10:57 +0530

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,47 @@
-import {
-  CallControls,
-  CallingState,
-  SpeakerLayout,
-  StreamCall,
-  StreamTheme,
-  StreamVideo,
-  StreamVideoClient,
-  useCallStateHooks,
-  User,
-} from '@stream-io/video-react-sdk';
-
-import '@stream-io/video-react-sdk/dist/css/styles.css';
-import './styles.css';
-// import {DoctorCard} from "./DoctorCard"
-
-const apiKey = 'mvxc2ac3sygv'; // the API key can be found in the "Credentials" section
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiSGFyc2hpdCJ9.kWcdQM0kEKBW6bGO8NMbOAMc4P-v-ALeRAobeGZXIKM'
-const userId = 'Harshit'; // the user id can be found in the "Credentials" section
-const callId = 'firstcall'; // the call id can be found in the "Credentials" section
-
-const user: User = {
-  id: userId,
-  name: 'User',
-  image: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
-};
-
-const client = new StreamVideoClient({ apiKey, user, token });
-const call = client.call('default', callId);
-call.join({ create: true });
+import React, { useState } from "react";
+import { VideoCall } from "./VideoCall.tsx";
 
 export default function App() {
-  return (
-    <StreamVideo client={client}>
-      <StreamCall call={call}>
-        <MyUILayout />
-      </StreamCall>
-    </StreamVideo>
-  );
-}
+  const [callId, setCallId] = useState('');
+  const [username, setUsername] = useState('');
+  const [showVideoCall, setShowVideoCall] = useState(false);
 
-export const MyUILayout = () => {
-  const { useCallCallingState } = useCallStateHooks();
-  const callingState = useCallCallingState();
+  const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'callId') {
+      setCallId(value);
+    }
+  }
 
-  if (callingState !== CallingState.JOINED) {
-    return <div className="flex items-center justify-center h-screen">
-    <div className="flex gap-4">
-      <div className="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full">
-        <svg viewBox="0 0 24 24" fill="currentColor" height="1em" width="1em" className="animate-ping">
-          <path d="M12.001 4.8c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624C13.666 10.618 15.027 12 18.001 12c3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C16.337 6.182 14.976 4.8 12.001 4.8zm-6 7.2c-3.2 0-5.2 1.6-6 4.8 1.2-1.6 2.6-2.2 4.2-1.8.913.228 1.565.89 2.288 1.624 1.177 1.194 2.538 2.576 5.512 2.576 3.2 0 5.2-1.6 6-4.8-1.2 1.6-2.6 2.2-4.2 1.8-.913-.228-1.565-.89-2.288-1.624C10.337 13.382 8.976 12 6.001 12z"></path>
-        </svg>
-      </div>
-    </div>
-  </div>;;
+  const handleFormSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setShowVideoCall(true);
   }
 
   return (
-    <StreamTheme>
-      <SpeakerLayout participantsBarPosition='bottom' />
-      <CallControls />
-    </StreamTheme>
+    <div>
+      {!showVideoCall ? (
+        <form onSubmit={handleFormSubmit}>
+          <input 
+            type="text" 
+            placeholder="Username" 
+            name="username" 
+            value={username} 
+            onChange={handleOnChange} 
+          />
+          <input 
+            type="text" 
+            placeholder="Call Id" 
+            name="callId" 
+            value={callId} 
+            onChange={handleOnChange} 
+          />
+          <button type="submit">Join Call</button>
+        </form>
+      ) : null}
+      {showVideoCall && <VideoCall callId={callId} username={username} />}
+    </div>
   );
-};
+}
---
+++ src/MyUILayout.tsx
@@ -0,0 +1,24 @@
+import { CallingState, CallControls, SpeakerLayout, StreamTheme, useCallStateHooks } from '@stream-io/video-react-sdk';
+
+export const MyUILayout = () => {
+  const { useCallCallingState } = useCallStateHooks();
+  const callingState = useCallCallingState();
+
+  if (callingState !== CallingState.JOINED) {
+    return (
+      <div className="loading">
+        <svg width="64px" height="48px">
+          <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+          <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+        </svg>
+      </div>
+    );
+  }
+
+  return (
+    <StreamTheme>
+      <SpeakerLayout participantsBarPosition='bottom' />
+      <CallControls />
+    </StreamTheme>
+  );
+};
